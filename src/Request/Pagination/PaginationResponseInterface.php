16ff64231be659aa4c620c2aacd7cae768a0cec1
Andrey Borodulin <borodulin@gmail.com>
2022-12-06 21:30:09 +0500

    refactor paginated

--- src/Request/Pagination/PaginationResponseInterface.php
+++ src/Request/Pagination/PaginationResponseInterface.php
@@ -6,13 +6,4 @@ namespace Borodulin\PresenterBundle\Request\Pagination;
 
 interface PaginationResponseInterface
 {
-    public function getTotalCount(): int;
-
-    public function getPage(): int;
-
-    public function getPageCount(): int;
-
-    public function getPageSize(): int;
-
-    public function getItems(): array;
 }
